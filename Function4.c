09f85e9b613fed2044aab8e8bdfaf4aae3f6d145
Vivek Dhore <vivekdhore2025@gmail.com>
2026-02-22 22:24:28 +0530

    Add files via upload

---
+++ Function4.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+int Addition(int iNo1 , int iNo2)
+{
+    int Sum = 0 ;
+    Sum = iNo1 + iNo2;
+    return Sum;
+}
+
+int Subtraction(int Data1,int Data2)
+{
+    int Result = 0 ;
+    Result = Data1 - Data2;
+    return Result;
+}
+int Multiple(int No1  , int No2)
+{
+    int Ans  = 0 ;
+    Ans  = No1 * No2;
+    return Ans ;
+}
+int Division()
+
+    
+int main()
+    {
+    int iValue1 = 12 ; 
+    int iValue2 = 12 ;
+    int Ans = 0;
+
+    Ans  = Addition(iValue1 , iValue2);
+    printf("Addition is  : %d\n",Ans);
+    Ans  = Subtraction(iValue1 , iValue2);
+    printf("Subtraction is : %d\n",Ans);
+    Ans  = Multiple(iValue1 , iValue2);
+    printf("Multiplication  is : %d",Ans);
+
+
+
+    return 0 ; 
+}
+
+// Home work : Ajay Mittal 
+// 1 Capter vacne 
+// Caper 2 Vacne 
+// Statement : program madhe lihlel Vakya  Capter 3
+//  capter 5 : function 
+
+  
