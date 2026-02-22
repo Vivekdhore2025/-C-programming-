9be9fabf45add65d7712e56622deda154c9547b0
Vivek Dhore <vivekdhore2025@gmail.com>
2026-02-22 22:08:23 +0530

    Add files via upload

---
+++ Function2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() // entry point function where  program execution start 
+{
+     int Value1 = 11;// its variable use for my function // local variable //personal watch
+     int Value2 = 10;
+     int Ans    = 0;   // Variable  creation
+
+    Ans = Value1 + Value2;
+    
+    printf("%d",Ans);   // %d for integer 
+    return 0; 
+ // writen 0 is  indicate to os program is successfull executed
+}
+
+// programming  logic written inside a seprate function
+// don't write any logic inside main function.
+
+
