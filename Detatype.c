d51d9f061f79eae4bb715c278e80dd10a1c1ff44
Vivek Dhore <vivekdhore2025@gmail.com>
2026-02-22 22:04:48 +0530

    Add files via upload

---
+++ Detatype.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+
+    char Division = 'A';
+    int Age = 12;
+    float Mark = 74.00;
+    double Percentage = 85.345f;
+
+    printf("%c\n",Division);
+    printf("%d\n",12);
+    printf("%f\n",Mark);
+    printf("%f\n",Percentage);
+
+    return 0;
+}
