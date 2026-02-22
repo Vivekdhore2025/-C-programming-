4a5656b410e4d2471959a20847fff45fc271b2c0
Vivek Dhore <vivekdhore2025@gmail.com>
2026-02-22 22:01:27 +0530

    Add files via upload

---
+++ Deta.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int Data  = 21;
+int main()
+{
+char cValue = 'A';
+int iValue  = 11;
+float fValue = 67.4f;
+double dValue = 78.34455;
+
+printf("%c\n",cValue);
+printf("%d\n",cValue);
+printf("%f\n",fValue);
+printf("%f\n",dValue);
+printf("%d\n",Data);
+    return 0 ; 
+}
